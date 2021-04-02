bd5678cd4384ccecd1f0f40087e69af9ba869bc6
Solvismara <34425392+Solvismara@users.noreply.github.com>
2021-04-02 11:07:02 +0000

    Update vendor.js

--- vendor.js
+++ vendor.js
@@ -1,17 +1,25 @@
-const monsterHealthBar = document.getElementById('monster-health');
-const playerHealthBar = document.getElementById('player-health');
-const bonusLifeEl = document.getElementById('bonus-life');
+const monsterHealthBar = document.getElementById("monster-health");
+const playerHealthBar = document.getElementById("player-health");
+const playerStaminaBar = document.getElementById("player-stamina");
+const totalmoney = document.getElementById("player-cash");
+const bonusLifeEl = document.getElementById("bonus-life");
 
-const attackBtn = document.getElementById('attack-btn');
-const strongAttackBtn = document.getElementById('strong-attack-btn');
-const healBtn = document.getElementById('heal-btn');
-const logBtn = document.getElementById('log-btn');
+const attackBtn = document.getElementById("attack-btn");
+const strongAttackBtn = document.getElementById("strong-attack-btn");
+const healBtn = document.getElementById("heal-btn");
+const staminaBTN = document.getElementById("stamina-btn");
+const logBtn = document.getElementById("log-btn");
 
 function adjustHealthBars(maxLife) {
   monsterHealthBar.max = maxLife;
   monsterHealthBar.value = maxLife;
   playerHealthBar.max = maxLife;
   playerHealthBar.value = maxLife;
+}
+
+function adjustStaminaBars(maxStamina) {
+  playerStaminaBar.max = maxStamina;
+  playerStaminaBar.vale = maxStamina;
 }
 
 function dealMonsterDamage(damage) {
@@ -26,8 +34,24 @@ function dealPlayerDamage(damage) {
   return dealtDamage;
 }
 
+function staminaCost(stamina) {
+  const staminaCost = Math.random() * stamina;
+  playerStaminaBar.value = +playerStaminaBar.value - staminaCost;
+  return staminaCost;
+}
+
+function spendMoney(cost) {
+  const moneySpent = cost;
+  totalmoney.value = +totalmoney.value - moneySpent;
+  return moneySpent;
+}
+
 function increasePlayerHealth(healValue) {
   playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function increasePlayerStamina(staminaValue) {
+  playerStaminaBar.value = +playerStaminaBar.value + staminaValue;
 }
 
 function resetGame(value) {
